ad1444ee932d54f14547ff51a801160ab3ca795f
Ren Aloyan <66231769+renaloyan@users.noreply.github.com>
2020-08-28 23:47:56 +0800

    added new functions

--- app/src/main/java/com/algobty/educare/OnBoarding.java
+++ app/src/main/java/com/algobty/educare/OnBoarding.java
@@ -3,8 +3,14 @@ package com.algobty.educare;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -12,17 +18,22 @@ public class OnBoarding extends AppCompatActivity {
 
     ViewPager viewPager;
     LinearLayout dotsLayout;
+    Button getStartedBtn;
 
     SliderAdapter sliderAdapter;
     TextView[] dots;
+    Animation animation;
+    int currentPos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_on_boarding);
 
         viewPager = findViewById(R.id.slider);
         dotsLayout = findViewById(R.id.dots);
+        getStartedBtn = findViewById(R.id.lets_started_btn);
 
         //call adapter
         sliderAdapter = new SliderAdapter(this);
@@ -30,6 +41,15 @@ public class OnBoarding extends AppCompatActivity {
 
         addDots(0);
         viewPager.addOnPageChangeListener(changeListener);
+    }
+
+    public void skip(View view){
+        startActivity(new Intent(getApplicationContext(), Home.class));
+        finish();
+    }
+
+    public void next(View view){
+        viewPager.setCurrentItem(currentPos + 1);
     }
 
     private void addDots(int position) {
@@ -61,6 +81,23 @@ public class OnBoarding extends AppCompatActivity {
         @Override
         public void onPageSelected(int position) {
             addDots(position);
+            currentPos = position;
+
+            if (position == 0){
+                getStartedBtn.setVisibility(View.INVISIBLE);
+            }
+            else if (position == 1){
+                getStartedBtn.setVisibility(View.INVISIBLE);
+            }
+            else if (position == 2){
+                getStartedBtn.setVisibility(View.INVISIBLE);
+            }
+            else {
+                animation = AnimationUtils.loadAnimation(OnBoarding.this, R.anim.bottom_anim);
+                getStartedBtn.setAnimation(animation);
+                getStartedBtn.setVisibility(View.VISIBLE);
+            }
+
         }
 
         @Override
